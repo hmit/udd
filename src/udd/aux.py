eac76474dcad975d143c19b345f880c83ab0b229
neronus-guest <neronus-guest@3b15d4d3-bb24-0410-9696-dc0fab150647>
2008-07-24 20:23:06 +0000

    Deleted some now redundant functions/methods
    git-svn-id: svn+ssh://svn.debian.org/svn/collab-qa/udd@931 3b15d4d3-bb24-0410-9696-dc0fab150647

--- src/udd/aux.py
+++ src/udd/aux.py
@@ -18,28 +18,6 @@ def null_or_quote(dict, key):
   else:
     return 'NULL'
 
-def get_archs(conn):
-  """Return a dicitionary, mapping from architecture names to their ids.
-
-  This mapping is retrivied from the connection <conn>"""
-  cur = conn.cursor()
-  cur.execute("SELECT * from arch_ids")
-  result = {}
-  for row in cur.fetchall():
-    result[row[1]] = row[0]
-  return result
-
-def get_distrs(conn):
-  """Return a dicitionary, mapping from distribution names to their ids.
-
-  This mapping is retrivied from the connection <conn>"""
-  cur = conn.cursor()
-  cur.execute("SELECT * from distr_ids")
-  result = {}
-  for row in cur.fetchall():
-    result[row[1]] = row[0]
-  return result
-
 class ConfigException(Exception):
   def __init__(self, message):
     Exception(self)
@@ -85,28 +63,8 @@ def load_config(str):
 
   return config
 
-def insert_distr(conn, distr_name):
-  "Insert distribution <distr_name> into DB"
-  cur = conn.cursor()
-  cur.execute("INSERT INTO distr_ids (name) VALUES ('%s')" % distr_name)
-
 def print_debug(*args):
   "Print arguments to stdout if debug is set to something that evaluates to true"
   if debug:
     sys.stdout.write(*args)
     sys.stdout.write("\n")
-
-class BufferedLineReader:
-  """Provides an iterator over the input of the specified file."""
-  def __init__(self, file, cache_size):
-    self.cache_size = cache_size
-    self.file = file
-
-  def __iter__(self):
-    while True:
-      lines = self.file.readlines(self.cache_size)
-      if len(lines) == 0:
-	break
-      for line in lines:
-	yield line
-
