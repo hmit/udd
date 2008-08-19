066c6e7343891cfe5d899f39b3eab3b023952115
neronus-guest <neronus-guest@3b15d4d3-bb24-0410-9696-dc0fab150647>
2008-08-19 09:41:49 +0000

    Moved code from udd-update.py to udd-dispatch.py
    git-svn-id: svn+ssh://svn.debian.org/svn/collab-qa/udd@1119 3b15d4d3-bb24-0410-9696-dc0fab150647

--- src/udd-dispatch.py
+++ src/udd-dispatch.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Last-Modified: <Sun Aug 17 12:03:34 2008>
+# Last-Modified: <Tue Aug 19 09:38:06 2008>
 
 """Dispatch udd gatherers
 
@@ -20,18 +20,13 @@ if __name__ == '__main__':
     sys.exit(1)
 
   command = sys.argv[2]
-  if command not in ('run', 'setup', 'drop', 'tables'):
-    sys.stderr.write("command has to be one of 'run', 'setup', 'drop' and 'tables'\n")
+  if command not in ('run', 'setup', 'drop', 'tables', 'update'):
+    sys.stderr.write("command has to be one of 'run', 'setup', 'drop', 'update' and 'tables'\n")
     sys.exit(1)
 
-  # Check the configuration
   config = udd.aux.load_config(open(sys.argv[1]).read())
 
   types = config['general']['types']
-
-  for src in sys.argv[3:]:
-    if not src in config:
-      raise udd.aux.ConfigException("%s is not specified in %s" % (src, sys.argv[1]))
 
   connection = udd.aux.open_connection(config)
 
@@ -39,29 +34,34 @@ if __name__ == '__main__':
   for src in sys.argv[3:]:
     src_config = config[src]
     type = src_config['type']
-    if not type in types:
-      print "No script specified for type " + src['type']
-      sys.exit(1)
-
-
-    (src_command,rest) = types[type].split(None, 1)
-    
     udd.aux.lock(config, src)
     try:
-      if src_command == "exec":
-	system(rest + " " + sys.argv[1] + " " + sys.argv[2] + " " + src)
-      elif src_command == "module":
-	exec("import " + rest)
-	exec "gatherer = " + rest + ".get_gatherer(connection, config, src)"
-	if command == 'tables':
-	  exec "tables = gatherer.%s()" % command
-	  print "\n".join(tables)
-	else:
-	  exec "gatherer.%s()" % command
-      if 'timestamp-folder' in config['general']:
-	f = open(os.path.join(config['general']['timestamp-folder'], src+".dispatch"), "w")
-	f.write(asctime())
-	f.close()
+      # If the command is update, we need a special case. Otherwise we can just use the gatherer's methods
+      if command == 'update':
+	if "update-command" in src_config:
+	  result = system(src_config['update-command']) 
+	  if result != 0:
+	    sys.exit(result)
+	  if 'timestamp-folder' in config['general']:
+	    f = open(os.path.join(config['general']['timestamp-folder'], src+".update"), "w")
+	    f.write(asctime())
+	    f.close()
+      else:
+	(src_command,rest) = types[type].split(None, 1)
+	if src_command == "exec":
+	  system(rest + " " + sys.argv[1] + " " + sys.argv[2] + " " + src)
+	elif src_command == "module":
+	  exec("import " + rest)
+	  exec "gatherer = " + rest + ".get_gatherer(connection, config, src)"
+	  if command == 'tables':
+	    exec "tables = gatherer.%s()" % command
+	    print "\n".join(tables)
+	  else:
+	    exec "gatherer.%s()" % command
+	if 'timestamp-folder' in config['general']:
+	  f = open(os.path.join(config['general']['timestamp-folder'], src+".dispatch"), "w")
+	  f.write(asctime())
+	  f.close()
     except:
       udd.aux.unlock(config, src)
       raise
