c7d0297b1525b4d5d7424cedf0169b55ab04a3b3
lucas <lucas@3b15d4d3-bb24-0410-9696-dc0fab150647>
2008-09-07 19:11:42 +0000

    added script to rsync distro
    git-svn-id: svn+ssh://svn.debian.org/svn/collab-qa/udd@1223 3b15d4d3-bb24-0410-9696-dc0fab150647

---
+++ scripts/sync-dist.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# usage:
+# /org/udd.debian.net/udd/scripts/sync-dist.sh archive.ubuntu.com::ubuntu/dists/intrepid /org/udd.debian.net/mirrors/ubuntu/intrepid
+
+[ -d $2 ] || mkdir -p $2
+rsync -rd --no-motd --include "Packages.gz" --include "Sources.gz" --include "**/" --exclude "*" $1 $2
