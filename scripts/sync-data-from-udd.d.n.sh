5423b769d8e1846288b014e4670997eac056ee3e
lucas <lucas@3b15d4d3-bb24-0410-9696-dc0fab150647>
2008-09-08 07:06:00 +0000

    more docs
    git-svn-id: svn+ssh://svn.debian.org/svn/collab-qa/udd@1237 3b15d4d3-bb24-0410-9696-dc0fab150647

--- scripts/sync-data-from-udd.d.n.sh
+++ scripts/sync-data-from-udd.d.n.sh
@@ -4,12 +4,15 @@
 
 # directory where to put the mirrors 
 # (usually /org/udd.debian.net/mirrors/ )
+TARGET=$1
+
+# You will probably need to symlink /org/bugs.debian.org to
+# /org/udd.debian.net/mirrors/bugs.debian.org
 
 if [ $# -ne 1 ]; then
 	echo "missing arg"
 	exit 1
 fi
-TARGET=$1
 
 rsync -avzP \
 udd.debian.net:/org/udd.debian.net/mirrors/{qa.debian.org,bugs.debian.org} \
