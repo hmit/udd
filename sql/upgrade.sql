15845c3b37ba9312c14a836da40c79b11f689024
lucas <lucas@3b15d4d3-bb24-0410-9696-dc0fab150647>
2008-09-26 20:32:07 +0000

    fix comments
    git-svn-id: svn+ssh://svn.debian.org/svn/collab-qa/udd@1296 3b15d4d3-bb24-0410-9696-dc0fab150647

--- sql/upgrade.sql
+++ sql/upgrade.sql
@@ -1,4 +1,4 @@
-# 2008-09-22: Change severity from text to bugs_severity enum:
+-- 2008-09-22: Change severity from text to bugs_severity enum:
 CREATE TYPE bugs_severity AS ENUM ('fixed', 'wishlist', 'minor', 'normal', 'important', 'serious', 'grave', 'critical');
 ALTER TABLE bugs add severity2 bugs_severity;
 UPDATE bugs set severity2 = severity::bugs_severity;
@@ -9,10 +9,10 @@ UPDATE archived_bugs set severity2 = severity::bugs_severity;
 ALTER TABLE archived_bugs drop severity;
 ALTER TABLE archived_bugs rename severity2 to severity;
 
-# 2008-09-22: Add a affects_experimental to bugs and archived_bugs
+-- 2008-09-22: Add a affects_experimental to bugs and archived_bugs
 ALTER TABLE bugs add affects_experimental boolean;
 ALTER TABLE archived_bugs add affects_experimental boolean;
 
-# 2008-09-22: Properly name affects_stable and affects_unstable
+-- 2008-09-22: Properly name affects_stable and affects_unstable
 ALTER TABLE archived_bugs rename affects_sarchived_table to affects_stable;
 ALTER TABLE archived_bugs rename affects_unsarchived_table to affects_unstable;
