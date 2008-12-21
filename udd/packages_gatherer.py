b69ab6b544b126fcc8ae15e9eca923187d57e30b
lucas <lucas@3b15d4d3-bb24-0410-9696-dc0fab150647>
2008-12-21 22:31:40 +0000

    complete the addition of Breaks
    git-svn-id: svn+ssh://svn.debian.org/svn/collab-qa/udd@1356 3b15d4d3-bb24-0410-9696-dc0fab150647

--- udd/packages_gatherer.py
+++ udd/packages_gatherer.py
@@ -170,7 +170,7 @@ class packages_gatherer(gatherer):
 	  VALUES
 	    ( $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15,
 	      $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28,
-	      $29, $30, $31, '%s', '%s', '%s')
+	      $29, $30, $31, $32, '%s', '%s', '%s')
 	    """ %  (table, self._distr, src_cfg['release'], comp))
 #	  aux.print_debug("Reading file " + path)
 	  # Copy content from gzipped file to temporary file, so that apt_pkg is
