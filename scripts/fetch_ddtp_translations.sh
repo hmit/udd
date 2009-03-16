23b6bb502570de4db9a51c0ef1622cf1704a3568
tille <tille@3b15d4d3-bb24-0410-9696-dc0fab150647>
2009-03-16 09:19:26 +0000

    DDTP has currentl yonly unzipped files with Version numbers, this will change in the very newar future but for the moment we need to zip them on our own
    git-svn-id: svn+ssh://svn.debian.org/svn/collab-qa/udd@1407 3b15d4d3-bb24-0410-9696-dc0fab150647

--- scripts/fetch_ddtp_translations.sh
+++ scripts/fetch_ddtp_translations.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-set -x
 TARGETPATH=`grep "^\s*path:" /org/udd.debian.net/udd/config_ddtp.yaml | sed 's/^\s*path:\s*//'`
 RELEASES=`grep "^\s*releases:" /org/udd.debian.net/udd/config_ddtp.yaml | sed 's/^\s*releases:\s*//'`
 MIRROR=`grep "^\s*mirror:" /org/udd.debian.net/udd/config_ddtp.yaml | sed 's/^\s*mirror:\s*//'`
@@ -8,8 +7,10 @@ HTTPMIRROR="http://$MIRROR"
 # rm -rf "$TARGETPATH"
 for rel in $RELEASES; do
     TARGETDIR="$TARGETPATH"/${rel}
+    rm -rf "$TARGETDIR"
     [ -d $TARGETDIR ] || mkdir -p $TARGETDIR
-    `dirname $0`/getlinks.pl "$HTTPMIRROR"/dists/${rel}/main/i18n/ "$TARGETPATH"/${rel} 'Translation-.*' #\.gz$'
+    `dirname $0`/getlinks.pl "$HTTPMIRROR"/dists/${rel}/main/i18n/ "$TARGETPATH"/${rel} 'Translation-.*' # \.gz$'
+    gzip "$TARGETPATH"/${rel}/Translation-*
     # rsync -a --no-motd --include "Translation-*.gz" --exclude "*" "$RSYNCMIRROR"/dists/${rel}/main/i18n/ $TARGETDIR
 done
 
--- scripts/fetch_ftpnew.sh
+++ scripts/fetch_ftpnew.sh
@@ -5,4 +5,8 @@ rm -rf $TARGETDIR/*
 wget -q http://ftp-master.debian.org/new.822 -O ${TARGETDIR}/new.822
 cd $TARGETDIR
 wget -q -r -N --level=2 --no-parent --no-directories http://ftp-master.debian.org/new/
+# Some large packages do contain e huge list of files which just consumes space in our
+# cache - so simply delete these entries which are of no use here
+#  sed -i '/^[-dlrwx]\+ root\/root/d' ${TARGETDIR}/*.html
+# Finally it might be better to keep originals ...
 rm -f $TARGETDIR/index.html*
