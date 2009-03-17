f967cd40adc29c0c706fc29f694a42f888fa8c6d
tille <tille@3b15d4d3-bb24-0410-9696-dc0fab150647>
2009-03-17 06:57:18 +0000

    ddtp server now provides gzipped translation files
    git-svn-id: svn+ssh://svn.debian.org/svn/collab-qa/udd@1409 3b15d4d3-bb24-0410-9696-dc0fab150647

--- scripts/fetch_ddtp_translations.sh
+++ scripts/fetch_ddtp_translations.sh
@@ -9,8 +9,11 @@ for rel in $RELEASES; do
     TARGETDIR="$TARGETPATH"/${rel}
     rm -rf "$TARGETDIR"
     [ -d $TARGETDIR ] || mkdir -p $TARGETDIR
-    `dirname $0`/getlinks.pl "$HTTPMIRROR"/dists/${rel}/main/i18n/ "$TARGETPATH"/${rel} 'Translation-.*' # \.gz$'
-    gzip "$TARGETPATH"/${rel}/Translation-*
+    `dirname $0`/getlinks.pl "$HTTPMIRROR"/dists/${rel}/main/i18n/ "$TARGETPATH"/${rel} 'Translation-.*\.gz$'
+    ## The rsync-able Translations do not (yet) contain package version info
+    ## This might happen later but it requires deeper changes in several tools
+    ## including apt - so we have to download via http from ddtp directly which
+    ## does not support rsync
     # rsync -a --no-motd --include "Translation-*.gz" --exclude "*" "$RSYNCMIRROR"/dists/${rel}/main/i18n/ $TARGETDIR
 done
 
