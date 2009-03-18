90a55c9b4b2c3c36b391073ed0664526b288b8b4
lucas <lucas@3b15d4d3-bb24-0410-9696-dc0fab150647>
2009-03-18 20:00:25 +0000

    merged ddtp.sql into setup.sql and upgrade.sql ; removed it
    git-svn-id: svn+ssh://svn.debian.org/svn/collab-qa/udd@1411 3b15d4d3-bb24-0410-9696-dc0fab150647

--- sql/upgrade.sql
+++ sql/upgrade.sql
@@ -74,3 +74,28 @@ ALTER TABLE migrations ALTER COLUMN testing_version TYPE debversion;
 ALTER TABLE migrations ALTER COLUMN unstable_version TYPE debversion;
 ALTER TABLE migrations ALTER COLUMN sync_version TYPE debversion;
 ALTER TABLE upload_history ALTER COLUMN version TYPE debversion;
+-- 2009-03-18
+CREATE TABLE ddtp (
+       package      text,
+       distribution text,
+       release      text,
+       component    text,   -- == 'main' for the moment
+       version      text,   -- different versions for a package might exist because some archs
+                            -- might have problems with newer versions if a new version comes
+                            -- with a new description we might have different translations for
+                            -- a (package, distribution, release, component, language) key so
+                            -- we also need to store the version number of a package translation
+                            -- In case there are different versions with an identical description
+                            -- this field will hold the highest version number according to
+                            -- dpkg --compare-versions
+       language     text,
+       description  text,
+       long_description text,
+       md5sum       text,   -- md5 sum of the original English description as it is used
+                            -- in DDTP.  This is obtained via
+                            --   md5(description || E'\n' || long_description || E'\n')
+                            -- from packages table
+    PRIMARY KEY (package, distribution, release, component, version, language)
+);
+
+GRANT SELECT ON ddtp TO PUBLIC;
