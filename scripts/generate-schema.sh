6164a965f54ede907f0802b93220c5d7fbd7e7d7
lucas <lucas@3b15d4d3-bb24-0410-9696-dc0fab150647>
2009-07-30 00:14:16 +0000

    use private copy of pg_autodoc to generate the schema
    git-svn-id: svn+ssh://svn.debian.org/svn/collab-qa/udd@1545 3b15d4d3-bb24-0410-9696-dc0fab150647

--- scripts/generate-schema.sh
+++ scripts/generate-schema.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 set -e
-set -x
 mkdir -p /org/udd.debian.org/udd/web/schema
 cd /org/udd.debian.org/udd/web/schema
-postgresql_autodoc -d udd -p 5441 -t html
-postgresql_autodoc -d udd -p 5441 -t dot
+P=/org/udd.debian.org/udd/scripts/pg_autodoc/
+$P/postgresql_autodoc -l $P -d udd -p 5441 -t html | grep -v 'Producing '
+$P/postgresql_autodoc -l $P -d udd -p 5441 -t dot | grep -v 'Producing '
 dot -Tpng udd.dot > udd.png
+dot -Tsvg udd.dot > udd.svg
