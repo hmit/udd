0b117b9dabf1a1c605d76fc538516b97d5010641
lucas <lucas@3b15d4d3-bb24-0410-9696-dc0fab150647>
2009-09-05 12:54:42 +0000

    exclude ldap from dump
    git-svn-id: svn+ssh://svn.debian.org/svn/collab-qa/udd@1572 3b15d4d3-bb24-0410-9696-dc0fab150647

--- scripts/dump-db.sh
+++ scripts/dump-db.sh
@@ -3,5 +3,5 @@
 set -e
 cd /org/udd.debian.org/udd/web/
 umask 022
-pg_dump --no-owner -p 5441 udd | gzip > udd.sql.gz.new
+pg_dump --no-owner -p 5441 -T ldap udd | gzip > udd.sql.gz.new
 mv udd.sql.gz.new udd.sql.gz
