8ad2d2e56f9406f9854772ce1d2fd65addd88027
lucas <lucas@3b15d4d3-bb24-0410-9696-dc0fab150647>
2009-09-05 12:54:54 +0000

    filter more stuff from cron emails
    git-svn-id: svn+ssh://svn.debian.org/svn/collab-qa/udd@1573 3b15d4d3-bb24-0410-9696-dc0fab150647

--- scripts/filter-bugs-output.sh
+++ scripts/filter-bugs-output.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-grep -v -e ': unable to open /org/bugs.debian.org/versions/pkg/' -e 'Use of uninitialized value in hash slice at ' -e 'Could not read file for bug '
+grep -v -e ': unable to open /org/bugs.debian.org/versions/pkg/' -e 'Use of uninitialized value in hash slice at ' -e 'Could not read file for bug ' -e 'Unmatched () ' -e "Unmatched '<>' in " -e 'Use of uninitialized value in subroutine entry at'
--- scripts/filter-output.rb
+++ scripts/filter-output.rb
@@ -3,7 +3,9 @@
 LINES = [
   "Can't parse line 66715: /org/lintian.debian.org/root/collection/strings: line 37: strings/./usr/share/doc/HOWTO/it-html/ELF-HOWTO/: Is a directory",
   "Can't parse line 124359: strings: 'unpacked/./usr/share/man/man3/Lire': No such file",
-  'Duplicated key in language es:  gedit-plugins debian main squeeze 2.22.5-1 Conjunto de complementos de gedit 5edb9235bd149de240234eeb4065cff7'
+  'Duplicated key in language es:  gedit-plugins debian main squeeze 2.22.5-1 Conjunto de complementos de gedit 5edb9235bd149de240234eeb4065cff7',
+  /illegal package name Package: /,
+  /Skipping upload: /
 ]
 
 STDIN.each_line do |l|
