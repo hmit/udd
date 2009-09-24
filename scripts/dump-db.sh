bee94dc7e1b58a76287f48b8ed6aab8a2bea1f5b
lucas <lucas@3b15d4d3-bb24-0410-9696-dc0fab150647>
2009-09-24 16:03:40 +0000

    also ignore really_active_dds view
    git-svn-id: svn+ssh://svn.debian.org/svn/collab-qa/udd@1591 3b15d4d3-bb24-0410-9696-dc0fab150647

--- scripts/dump-db.sh
+++ scripts/dump-db.sh
@@ -3,5 +3,5 @@
 set -e
 cd /org/udd.debian.org/udd/web/
 umask 022
-pg_dump --no-owner -p 5441 -T ldap udd | gzip > udd.sql.gz.new
+pg_dump --no-owner -p 5441 -T ldap -T really_active_dds udd | gzip > udd.sql.gz.new
 mv udd.sql.gz.new udd.sql.gz
