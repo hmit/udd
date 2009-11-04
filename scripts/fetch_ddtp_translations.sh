ec491c3e26fa5b823d6cbdd0ecc28b369f3a7c48
tille <tille@3b15d4d3-bb24-0410-9696-dc0fab150647>
2009-11-04 21:35:23 +0000

    Make sure UDD will not be updated with non existant DDTP data by returning error code if download failed
    git-svn-id: svn+ssh://svn.debian.org/svn/collab-qa/udd@1620 3b15d4d3-bb24-0410-9696-dc0fab150647

--- scripts/fetch_ddtp_translations.sh
+++ scripts/fetch_ddtp_translations.sh
@@ -15,6 +15,13 @@ for rel in $RELEASES; do
     rm -rf "$TARGETDIR"
     [ -d $TARGETDIR ] || mkdir -p $TARGETDIR
     `dirname $0`/getlinks.pl "$HTTPMIRROR"/dists/${rel}/main/i18n/ "$TARGETPATH"/${rel} 'Translation-.*\.gz$'
+    # getlinks.pl always returns 0 independently from success so we have to verify that the target dir is
+    # not empty.
+    NUMFILES=`ls "$TARGETPATH"/${rel} | wc -l`
+    if [ $NUMFILES -le 0 ] ; then
+	echo "Downloading translation for release ${rel} failed. Stopped."
+	exit 66
+    fi
     ## The rsync-able Translations do not (yet) contain package version info
     ## This might happen later but it requires deeper changes in several tools
     ## including apt - so we have to download via http from ddtp directly which
