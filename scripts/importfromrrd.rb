d8fb165aa2d0fd4616d1cf5a092f604d5ac06e4d
lucas <lucas@3b15d4d3-bb24-0410-9696-dc0fab150647>
2009-12-21 18:01:05 +0000

    add script used to import data from rrd files
    git-svn-id: svn+ssh://svn.debian.org/svn/collab-qa/udd@1645 3b15d4d3-bb24-0410-9696-dc0fab150647

---
+++ scripts/importfromrrd.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/ruby -w
+# Used by DDPO
+
+require 'dbi'
+
+dbh = DBI::connect('DBI:Pg:dbname=udd;port=5441', 'udd')
+fields = [ 'vcstype_arch',
+  'vcstype_bzr' ,
+  'vcstype_cvs' ,
+  'vcstype_darcs' ,
+  'vcstype_git' ,
+  'vcstype_hg' ,
+  'vcstype_mtn' ,
+  'vcstype_svn' ,
+  'format_3native' ,
+  'format_3quilt' ]
+
+data = eval(IO::read('/home/lucas/out.sources'))
+data.each_pair do |date, values|
+  s = "insert into history.sources_count(time,"+
+     fields.join(",")+") VALUES(\'" + date + "\',"+fields.map { |f| values[f].nil? ? "NULL" : values[f] }.join(",") + ")"
+  p s
+  sth = dbh.prepare(s)
+  sth.execute
+end
