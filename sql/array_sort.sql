800d377053d13e97f55bbee48c9b1361c0638adf
tille <tille@3b15d4d3-bb24-0410-9696-dc0fab150647>
2010-02-05 10:31:38 +0000

    Table with release and release dates, function to order an array.
    git-svn-id: svn+ssh://svn.debian.org/svn/collab-qa/udd@1677 3b15d4d3-bb24-0410-9696-dc0fab150647

---
+++ sql/array_sort.sql
@@ -0,0 +1,16 @@
+/*****************************************************************************
+ * Sorting an array.  See                                                    * 
+ * http://www.postgres.cz/index.php/PostgreSQL_SQL_Tricks#General_array_sort *
+ *****************************************************************************/
+CREATE OR REPLACE FUNCTION array_sort (ANYARRAY)
+RETURNS ANYARRAY LANGUAGE SQL
+AS $$
+SELECT ARRAY(
+    SELECT $1[s.i] AS "foo"
+    FROM
+        generate_series(array_lower($1,1), array_upper($1,1)) AS s(i)
+    ORDER BY foo
+);
+$$;
+
+
