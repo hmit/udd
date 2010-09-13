3041b2e199f202177e9216a6e66a9c1d25cbd8fe
lucas <lucas@3b15d4d3-bb24-0410-9696-dc0fab150647>
2010-09-13 09:12:41 +0000

    improve bugs_rt_affects_* views
    git-svn-id: svn+ssh://svn.debian.org/svn/collab-qa/udd@1782 3b15d4d3-bb24-0410-9696-dc0fab150647

--- sql/setup.sql
+++ sql/setup.sql
@@ -271,8 +271,7 @@ WHERE affects_stable
 AND (id NOT IN (SELECT id FROM bugs_tags WHERE tag IN ('sid', 'sarge', 'etch', 'squeeze', 'experimental'))
 OR id IN (SELECT id FROM bugs_tags WHERE tag = 'lenny'))
 AND id NOT IN (select id FROM bugs_tags WHERE tag = 'lenny-ignore')
-AND ( package IN (SELECT DISTINCT package FROM packages_summary p WHERE release = 'lenny')
-OR source IN (SELECT DISTINCT source FROM sources WHERE release = 'lenny'));
+AND id in (select id from sources, bugs_packages where sources.source = bugs_packages.source and release='lenny');
 
 CREATE VIEW bugs_rt_affects_testing AS
 SELECT id, package, source FROM bugs
@@ -280,26 +279,22 @@ WHERE affects_testing
 AND (id NOT IN (SELECT id FROM bugs_tags WHERE tag IN ('sid', 'sarge', 'etch', 'lenny', 'experimental'))
 OR id IN (SELECT id FROM bugs_tags WHERE tag = 'squeeze'))
 AND id NOT IN (select id FROM bugs_tags WHERE tag = 'squeeze-ignore')
-AND ( package IN (SELECT DISTINCT package FROM packages_summary p WHERE release = 'squeeze')
-OR source IN (SELECT DISTINCT source FROM sources WHERE release = 'squeeze'));
+AND id in (select id from sources, bugs_packages where sources.source = bugs_packages.source and release='squeeze');
 
 CREATE VIEW bugs_rt_affects_unstable AS
 SELECT id, package, source FROM bugs
 WHERE affects_unstable 
 AND (id NOT IN (SELECT id FROM bugs_tags WHERE tag IN ('lenny', 'sarge', 'etch', 'squeeze', 'experimental'))
 OR id IN (SELECT id FROM bugs_tags WHERE tag = 'sid'))
-AND ( package IN (SELECT DISTINCT package FROM packages_summary p WHERE release = 'sid')
-OR source IN (SELECT DISTINCT source FROM sources WHERE release = 'sid'));
+AND id in (select id from sources, bugs_packages where sources.source = bugs_packages.source and release='sid');
 
 CREATE VIEW bugs_rt_affects_testing_and_unstable AS
 SELECT id, package, source FROM bugs
 WHERE affects_unstable AND affects_testing
 AND (id NOT IN (SELECT id FROM bugs_tags WHERE tag IN ('sarge', 'etch', 'lenny', 'experimental'))
 OR (id IN (SELECT id FROM bugs_tags WHERE tag = 'sid') AND id IN (SELECT id FROM bugs_tags WHERE tag = 'squeeze')))
-AND ( package IN (SELECT DISTINCT package FROM packages_summary p WHERE release = 'sid')
-OR source IN (SELECT DISTINCT source FROM sources WHERE release = 'sid'))
-AND ( package IN (SELECT DISTINCT package FROM packages_summary p WHERE release = 'squeeze')
-OR source IN (SELECT DISTINCT source FROM sources WHERE release = 'squeeze'));
+AND id in (select id from sources, bugs_packages where sources.source = bugs_packages.source and release='squeeze')
+AND id in (select id from sources, bugs_packages where sources.source = bugs_packages.source and release='sid');
 
 GRANT SELECT ON bugs TO PUBLIC;
 GRANT SELECT ON bugs_packages TO PUBLIC;
