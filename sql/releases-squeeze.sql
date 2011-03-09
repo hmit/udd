eb7b815e57e2b32f79e647962e6e6e860bb84ede
tille <tille@3b15d4d3-bb24-0410-9696-dc0fab150647>
2011-03-09 21:42:20 +0000

    Fix typo in release date
    git-svn-id: svn+ssh://svn.debian.org/svn/collab-qa/udd@1917 3b15d4d3-bb24-0410-9696-dc0fab150647

--- sql/releases-squeeze.sql
+++ sql/releases-squeeze.sql
@@ -3,9 +3,9 @@
  ****************************************************************************************/
 
 BEGIN;
-UPDATE releases SET releasedate = '2011-03-06' WHERE release = 'squeeze';
-UPDATE releases SET releasedate = '2011-03-06' WHERE release = 'squeeze-security';
-UPDATE releases SET releasedate = '2011-03-06' WHERE release = 'squeeze-proposed-updates';
+UPDATE releases SET releasedate = '2011-02-06' WHERE release = 'squeeze';
+UPDATE releases SET releasedate = '2011-02-06' WHERE release = 'squeeze-security';
+UPDATE releases SET releasedate = '2011-02-06' WHERE release = 'squeeze-proposed-updates';
 
 INSERT INTO releases VALUES ( 'wheezy',                  NULL,         700 );
 INSERT INTO releases VALUES ( 'wheezy-security',         NULL,         701 );
