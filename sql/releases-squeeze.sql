6707f21e7831c743d9474b31d13a09e91b13bedb
tille <tille@3b15d4d3-bb24-0410-9696-dc0fab150647>
2011-03-09 21:32:45 +0000

    Add release dates for Squeeze
    git-svn-id: svn+ssh://svn.debian.org/svn/collab-qa/udd@1916 3b15d4d3-bb24-0410-9696-dc0fab150647

---
+++ sql/releases-squeeze.sql
@@ -0,0 +1,13 @@
+/****************************************************************************************
+ * Update releases table after Squeeze release                                          *
+ ****************************************************************************************/
+
+BEGIN;
+UPDATE releases SET releasedate = '2011-03-06' WHERE release = 'squeeze';
+UPDATE releases SET releasedate = '2011-03-06' WHERE release = 'squeeze-security';
+UPDATE releases SET releasedate = '2011-03-06' WHERE release = 'squeeze-proposed-updates';
+
+INSERT INTO releases VALUES ( 'wheezy',                  NULL,         700 );
+INSERT INTO releases VALUES ( 'wheezy-security',         NULL,         701 );
+INSERT INTO releases VALUES ( 'wheezy-proposed-updates', NULL,         702 );
+COMMIT;
