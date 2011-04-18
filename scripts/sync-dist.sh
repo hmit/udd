ebbf2aa139658252e4cb15831215576ee90b49b7
tille <tille@3b15d4d3-bb24-0410-9696-dc0fab150647>
2011-04-18 12:47:42 +0000

    Do not try syncing dirs ".~tmp~"
    git-svn-id: svn+ssh://svn.debian.org/svn/collab-qa/udd@1936 3b15d4d3-bb24-0410-9696-dc0fab150647

--- scripts/sync-dist.sh
+++ scripts/sync-dist.sh
@@ -4,4 +4,4 @@
 # /org/udd.debian.net/udd/scripts/sync-dist.sh archive.ubuntu.com::ubuntu/dists/intrepid /org/udd.debian.net/mirrors/ubuntu/intrepid
 
 [ -d $2 ] || mkdir -p $2
-rsync -rd --no-motd --include "Packages.gz" --include "Sources.gz" --include "**/" --exclude "*" $1 $2
+rsync -rd --no-motd --include "Packages.gz" --include "Sources.gz" --include "**/" --exclude "*" --exclude ".~tmp~" $1 $2
