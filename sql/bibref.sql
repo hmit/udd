a70a985ae794c1a662b7230e5945fa79f5ded08a
tille <tille@3b15d4d3-bb24-0410-9696-dc0fab150647>
2012-03-26 22:08:25 +0000

    Add rank to bibref table structure
    git-svn-id: svn+ssh://svn.debian.org/svn/collab-qa/udd@2181 3b15d4d3-bb24-0410-9696-dc0fab150647

--- sql/bibref.sql
+++ sql/bibref.sql
@@ -1,27 +1,16 @@
--- http://upstream-metadata.debian.net/for_UDD/biblio.yaml
-
 BEGIN;
 
 DROP TABLE IF EXISTS bibref CASCADE;
 
 CREATE TABLE bibref (
-	package			text NOT NULL,
-	key			text NOT NULL,
-	value			text NOT NULL
-	-- , PRIMARY KEY (package,key) -- this helps preventing more than one times the same key for a single package
+	package	text NOT NULL,
+	key	text NOT NULL,
+	value	text NOT NULL,
+	rank    int  NOT NULL,
+	PRIMARY KEY (package,key,rank) -- this helps preventing more than one times the same key for a single package
 );
 
 GRANT SELECT ON bibref TO PUBLIC;
 
 COMMIT;
 
--- 'name'       --> 'package'
--- 'section'
--- 'maintainer'
--- 'maintainer_email'
--- 'version'
--- 'homepage'
--- 'description'
--- 'url'
--- 'large_image_url'
--- 'small_image_url'
