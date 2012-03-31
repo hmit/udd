4f2b699a976f4e45ce063a700cd027f56a8d8e61
tille <tille@3b15d4d3-bb24-0410-9696-dc0fab150647>
2012-03-31 16:42:46 +0000

    Set '' as default for package column in bibref table
    git-svn-id: svn+ssh://svn.debian.org/svn/collab-qa/udd@2200 3b15d4d3-bb24-0410-9696-dc0fab150647

--- sql/bibref.sql
+++ sql/bibref.sql
@@ -6,8 +6,9 @@ CREATE TABLE bibref (
 	source	text NOT NULL,
 	key	text NOT NULL,
 	value	text NOT NULL,
+	package text DEFAULT '',
 	rank    int  NOT NULL,
-	PRIMARY KEY (source,key,rank) -- this helps preventing more than one times the same key for a single package
+	PRIMARY KEY (source,key,package,rank) -- this helps preventing more than one times the same key for a single package
 );
 
 GRANT SELECT ON bibref TO PUBLIC;
