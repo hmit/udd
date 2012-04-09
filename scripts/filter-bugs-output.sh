2bea13f1a9e236056b578f9c27ad58a4d9c5c175
lucas <lucas@3b15d4d3-bb24-0410-9696-dc0fab150647>
2012-04-09 05:44:01 +0000

    fix typo
    git-svn-id: svn+ssh://svn.debian.org/svn/collab-qa/udd@2220 3b15d4d3-bb24-0410-9696-dc0fab150647

--- scripts/filter-bugs-output.sh
+++ scripts/filter-bugs-output.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 
-grep -v -e ': unable to open /org/bugs.debian.org/versions/pkg/' -e 'Use of uninitialized value in hash slice at ' -e 'Could not read file for bug ' -e 'Unmatched () ' -e "Unmatched '<>' in " -e 'Use of uninitialized value in subroutine entry at' -e 'Mail::Address::_tokenise' -e 'Mail::Address::parse' -e 'main::run(\'HASH' -e 'main::main() called at /org/udd.debian.org/udd/udd/bugs_gatherer.pl line 401' -e 'Wide character in print at /org/udd.debian.org/mirrors/bugs.debian.org/perl/Debbugs/Packages.pm line'
+grep -v -e ': unable to open /org/bugs.debian.org/versions/pkg/' -e 'Use of uninitialized value in hash slice at ' -e 'Could not read file for bug ' -e 'Unmatched () ' -e "Unmatched '<>' in " -e 'Use of uninitialized value in subroutine entry at' -e 'Mail::Address::_tokenise' -e 'Mail::Address::parse' -e "main::run('HASH" -e 'main::main() called at /org/udd.debian.org/udd/udd/bugs_gatherer.pl line 401' -e 'Wide character in print at /org/udd.debian.org/mirrors/bugs.debian.org/perl/Debbugs/Packages.pm line'
