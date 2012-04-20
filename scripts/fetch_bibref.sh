70937006a8d0fd6e96b6a58cab02951b7114de04
tille <tille@3b15d4d3-bb24-0410-9696-dc0fab150647>
2012-04-20 21:32:04 +0000

    No debug output
    git-svn-id: svn+ssh://svn.debian.org/svn/collab-qa/udd@2230 3b15d4d3-bb24-0410-9696-dc0fab150647

--- scripts/fetch_bibref.sh
+++ scripts/fetch_bibref.sh
@@ -5,7 +5,7 @@ set -e
 TARGETDIR=/org/udd.debian.org/mirrors/bibref
 FETCHURL=http://blends.debian.net/packages-metadata/packages-metadata.tar.bz2
 ARCHIVE=`basename $FETCHURL`
-set -x
+#set -x
 rm -rf $TARGETDIR
 mkdir -p $TARGETDIR
 wget -q ${FETCHURL} -O ${TARGETDIR}/${ARCHIVE}
