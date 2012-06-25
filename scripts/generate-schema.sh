b2bede6eb31ec862021870021c471dc7feb503e2
Lucas Nussbaum <lucas@debian.org>
2012-06-25 06:43:29 +0000

    fix generate-schema.sh (new paths and ports)

--- scripts/generate-schema.sh
+++ scripts/generate-schema.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 set -e
-mkdir -p /org/udd.debian.org/udd/web/schema
-cd /org/udd.debian.org/udd/web/schema
-P=/org/udd.debian.org/udd/scripts/pg_autodoc/
-$P/postgresql_autodoc -l $P -d udd -p 5441 -t html | grep -v 'Producing '
-$P/postgresql_autodoc -l $P -d udd -p 5441 -t dot | grep -v 'Producing '
+mkdir -p /srv/udd.debian.org/udd/web/schema
+cd /srv/udd.debian.org/udd/web/schema
+P=/srv/udd.debian.org/udd/scripts/pg_autodoc/
+$P/postgresql_autodoc -l $P -d udd -p 5452 -t html | grep -v 'Producing '
+$P/postgresql_autodoc -l $P -d udd -p 5452 -t dot | grep -v 'Producing '
 dot -Tpng udd.dot > udd.png
 dot -Tsvg udd.dot > udd.svg
