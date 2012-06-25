3969060ce9190726ed9bfd615da6d8da4b563b03
Lucas Nussbaum <lucas@debian.org>
2012-06-25 11:10:33 +0000

    fix path and ports

--- scripts/dump-db.sh
+++ scripts/dump-db.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 set -e
-cd /org/udd.debian.org/udd/web/
+cd /srv/udd.debian.org/udd/web/
 umask 022
-pg_dump --no-owner -p 5441 -n history udd | gzip > udd-history.sql.gz.new
+pg_dump --no-owner -p 5452 -n history udd | gzip > udd-history.sql.gz.new
 mv udd-history.sql.gz.new udd-history.sql.gz
-pg_dump --no-owner -p 5441 -T ldap -T really_active_dds -T pts udd | gzip > udd.sql.gz.new
+pg_dump --no-owner -p 5452 -T ldap -T really_active_dds -T pts udd | gzip > udd.sql.gz.new
 mv udd.sql.gz.new udd.sql.gz
