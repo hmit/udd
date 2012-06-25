4b68aa8486d7c853b2e9d3085e88ebd0ee53f792
Lucas Nussbaum <lucas@debian.org>
2012-06-25 06:34:46 +0000

    new paths

--- scripts/sync-carnivore.sh
+++ scripts/sync-carnivore.sh
@@ -2,6 +2,6 @@
 
 set -e
 umask 002
-pw=$(</org/udd.debian.org/udd-qa.debian.org-password)
-TARGET=/org/udd.debian.org/mirrors/qa.debian.org-carnivore-report
+pw=$(</srv/udd.debian.org/udd-qa.debian.org-password)
+TARGET=/srv/udd.debian.org/mirrors/qa.debian.org-carnivore-report
 echo http://udd:${pw}@qa.debian.org/carnivore-report | wget -q -O $TARGET -i -
