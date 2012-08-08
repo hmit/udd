44add0423032f43437dc605f8e958ffef43a1788
Andreas Tille <tille@debian.org>
2012-08-08 10:16:56 +0200

    enable cloning bug database to save diskspace and bandwidth for complete mirror of bugs.debian.org

---
+++ scripts/clone_udd_bugs_fetch.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+set -x
+
+TARGETDIR=/srv/udd.debian.org/mirrors/clone-bugs
+FETCHURL=http://udd.debian.org/udd-bugs.sql.gz
+
+rm -rf $TARGETDIR
+mkdir -p $TARGETDIR
+cd $TARGETDIR
+wget -q ${FETCHURL}
---
+++ scripts/clone_udd_bugs_inject.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# echo "Hallo Welt"
+set -x
+
+BUGSDUMP=/srv/udd.debian.org/mirrors/clone-bugs/udd-bugs.sql.gz
+
+unset LANG
+unset LC_ALL
+
+psql udd <<EOT
+DROP TABLE IF EXISTS bugs CASCADE;
+DROP TABLE IF EXISTS bugs_merged_with;
+DROP TABLE IF EXISTS bugs_blockedby;
+DROP TABLE IF EXISTS bugs_blocks;
+DROP TABLE IF EXISTS bugs_fixed_in;
+DROP TABLE IF EXISTS bugs_found_in;
+DROP TABLE IF EXISTS bugs_packages CASCADE;
+DROP TABLE IF EXISTS bugs_tags;
+DROP TABLE IF EXISTS bugs_usertags;
+EOT
+
+zcat $BUGSDUMP | psql udd
