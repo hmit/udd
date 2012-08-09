8b5a5a37b74a630ef39b41ede4cdb5e9e2f88a33
Iain Lane <laney@debian.org>
2012-08-09 17:03:55 +0100

    generate-schema.sh: Don't worry if the greps don't match

--- scripts/generate-schema.sh
+++ scripts/generate-schema.sh
@@ -4,7 +4,7 @@ set -e
 mkdir -p /srv/udd.debian.org/udd/web/schema
 cd /srv/udd.debian.org/udd/web/schema
 P=/srv/udd.debian.org/udd/scripts/pg_autodoc/
-$P/postgresql_autodoc -l $P -d udd -p 5452 -t html | grep -v 'Producing '
-$P/postgresql_autodoc -l $P -d udd -p 5452 -t dot | grep -v 'Producing '
+$P/postgresql_autodoc -l $P -d udd -p 5452 -t html | grep -v 'Producing ' || test $? -eq 1
+$P/postgresql_autodoc -l $P -d udd -p 5452 -t dot | grep -v 'Producing ' || test $? -eq 1
 dot -Tpng udd.dot > udd.png
 dot -Tsvg udd.dot > udd.svg
