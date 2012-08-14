3e7c2379c45605123b930f190b7e0db6db0f86ab
Andreas Tille <tille@debian.org>
2012-08-14 16:16:10 +0200

    Silence psql calls

--- scripts/clone_udd_bugs_inject.sh
+++ scripts/clone_udd_bugs_inject.sh
@@ -4,7 +4,7 @@ BUGSDUMP=/srv/udd.debian.org/mirrors/clone-bugs/udd-bugs.sql.gz
 unset LANG
 unset LC_ALL
 
-psql udd <<EOT
+psql --quiet udd <<EOT
 DROP TABLE IF EXISTS bugs CASCADE;
 DROP TABLE IF EXISTS bugs_merged_with;
 DROP TABLE IF EXISTS bugs_blockedby;
@@ -16,4 +16,4 @@ DROP TABLE IF EXISTS bugs_tags;
 DROP TABLE IF EXISTS bugs_usertags;
 EOT
 
-zcat $BUGSDUMP | psql udd
+zcat $BUGSDUMP | psql --quiet udd
