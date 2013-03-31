2b7edc11148fe36d22b2a3c3ed680fe334c584d4
Lucas Nussbaum <lucas@debian.org>
2013-03-31 14:16:05 +0000

    Commit some uncommitted changes to filter-bugs-output.

--- scripts/filter-bugs-output.sh
+++ scripts/filter-bugs-output.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-grep -v -e ': unable to open /srv/bugs.debian.org/versions/pkg/' -e 'Use of uninitialized value in hash slice at ' -e 'Could not read file for bug ' -e 'Unmatched () ' -e "Unmatched '<>' in " -e 'Use of uninitialized value in subroutine entry at' -e 'Mail::Address::_tokenise' -e 'Mail::Address::parse' -e "main::run('HASH" -e 'main::main() called at /srv/udd.debian.org/udd/udd/bugs_gatherer.pl line 401' -e 'Wide character in print at /srv/bugs.debian.org/perl/Debbugs/Packages.pm line' -e 'does not map to Unicode at'
+grep -v\
+ -e ': unable to open /srv/bugs.debian.org/versions/pkg/'\
+ -e 'Use of uninitialized value in hash slice at '\
+ -e 'Could not read file for bug '\
+ -e 'Unmatched () '\
+ -e "Unmatched '<>' in "\
+ -e 'Use of uninitialized value in subroutine entry at'\
+ -e 'Mail::Address::_tokenise'\
+ -e 'Mail::Address::parse'\
+ -e "main::run('HASH"\
+ -e "main::update_bugs('HASH"\
+ -e "main::update_bug('HASH"\
+ -e 'main::main() called at /srv/udd.debian.org/udd.*/udd/bugs_gatherer.pl line'\
+ -e 'Wide character in print at /srv/bugs.debian.org/perl/Debbugs/Packages.pm line'\
+ -e 'does not map to Unicode at'
