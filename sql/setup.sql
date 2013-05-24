de63604492b57f39e776a384fd753f3b44e427c5
Andreas Tille <tille@debian.org>
2013-05-24 16:19:53 +0200

    Restore content of previously existing separate file blends-prospective-packages.sql which was lost somehow

--- sql/setup.sql
+++ sql/setup.sql
@@ -1714,3 +1714,35 @@ CREATE OR REPLACE FUNCTION blends_metapackage_translations (text[]) RETURNS SETO
     WHERE p.package = ANY ($1)
  $$ LANGUAGE 'SQL';
 
+DROP TABLE IF EXISTS blends_prospectivepackages CASCADE;
+
+CREATE TABLE blends_prospectivepackages
+  (blend text,
+   package text,
+   source text,
+   maintainer text,
+   maintainer_name text,
+   maintainer_email text,
+   changed_by text,
+   changed_by_name text,
+   changed_by_email text,
+   uploaders text,
+   description text,
+   long_description text,
+   description_md5 text,
+   homepage text,
+   component text,
+   section text,
+   priority text,
+   vcs_type text,
+   vcs_url text,
+   vcs_browser text,
+   wnpp int,
+   wnpp_type text,
+   wnpp_desc text,
+   license text,
+   chlog_date text, -- time,
+   chlog_version debversion
+);
+
+GRANT SELECT ON blends_prospectivepackages TO PUBLIC;
