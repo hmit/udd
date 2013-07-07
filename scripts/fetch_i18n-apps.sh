924732d545fe66127893494d13735bad463219b1
Andreas Tille <tille@debian.org>
2013-07-07 05:52:02 +0200

    i18n became official (.org rather than .net)

--- scripts/fetch_i18n-apps.sh
+++ scripts/fetch_i18n-apps.sh
@@ -2,6 +2,6 @@
 TARGETDIR=/srv/udd.debian.org/mirrors/i18n-apps
 mkdir -p $TARGETDIR
 rm -rf $TARGETDIR/*
-wget -q http://i18n.debian.net/material/data/unstable.gz -O ${TARGETDIR}/sid.gz
-wget -q http://i18n.debian.net/material/data/testing.gz -O ${TARGETDIR}/wheezy.gz
+wget -q http://i18n.debian.org/material/data/unstable.gz -O ${TARGETDIR}/sid.gz
+wget -q http://i18n.debian.org/material/data/testing.gz -O ${TARGETDIR}/wheezy.gz
 
