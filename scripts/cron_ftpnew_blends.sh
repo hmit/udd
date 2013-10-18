f1ecc4906d54e04bb825336c338f17fc797ef7a6
Andreas Tille <tille@debian.org>
2013-10-18 14:09:30 +0200

    Add script that should be called in cron job to ensure proper sequence of updates for ftpnew, blends-prospective and blends-metadata

---
+++ scripts/cron_ftpnew_blends.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# To avoid duplicate entries in new_packages and blends_prospectivepackages
+# a defined sequence of imports has to be ensured.  So the following
+# importers are called in a sequence rather than at random cron times.
+
+UAR=/srv/udd.debian.org/udd/update-and-run.sh
+
+$UAR ftpnew
+$UAR blends-prospective
+$UAR blends-metadata
