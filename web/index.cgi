f805cf653f270fb9e997fbb4487f0a157f728871
Christophe Siraut <d@tobald.eu.org>
2013-12-07 12:43:48 +0100

    Dashboard improvements.

--- web/index.cgi
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/ruby
-
-require File.expand_path(File.dirname(__FILE__))+'/inc/page'
-
-page = Page.new({ :title => 'Ultimate Debian Database' })
-puts "Content-type: text/html\n\n"
-puts page.render("templates/index.erb")
---
+++ web/js/dmd.js
@@ -0,0 +1,54 @@
+/*!
+ * Debian Maintainer Dashboard Library
+ */
+$(function() {
+  $( "#email1" ).autocomplete({
+    source: "/dmd-emails.cgi",
+    select: function(event, ui) {
+        $("#email1").val(ui.item.value);
+        $("#searchForm").submit();
+    }
+  });
+  $( "#email2" ).autocomplete({
+    source: "/dmd-emails.cgi",
+    select: function(event, ui) {
+        $("#email2").val(ui.item.value);
+        $("#searchForm").submit();
+    }
+  });
+  $( "#email3" ).autocomplete({
+    source: "/dmd-emails.cgi",
+    select: function(event, ui) {
+        $("#email3").val(ui.item.value);
+        $("#searchForm").submit();
+    }
+  });
+});
+$(document).ready(function() {
+$("table.tablesorter").each(function(index) { $(this).tablesorter() });
+$("tbody.todos tr").each(function(index, elem) { if ($.cookie(elem.id) == '1') $(elem).hide(); });
+});
+function hide_todo(id) {
+  $.cookie(id, '1', { expires: 3650 });
+  $("tbody.todos tr#"+id).hide();
+}
+
+function reset_todos() {
+$("tbody.todos tr").each(function(index, elem) {
+ $.cookie(elem.id, null);
+ $(elem).show();
+});
+}
+
+function removeBlankFields(form) {
+	var inputs = form.getElementsByTagName("input");
+	var removeList = new Array();
+	for (var i=0; i<inputs.length; i++) {
+		if (inputs[i].value == "") {
+			removeList.push(inputs[i]);
+		}
+	}
+	for (x in removeList) {
+		removeList[x].parentNode.removeChild(removeList[x]);
+	}
+}
