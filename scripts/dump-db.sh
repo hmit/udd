5cc2d0a5c6809da1e5e70f663ac4d87120d82bdf
Ivo De Decker <ivo.dedecker@ugent.be>
2014-01-01 13:00:55 +0000

    move dumps to separate dir and touch .nobackup there
    This should exclude the dumps from the backups, at the request of DSA

--- scripts/dump-db.sh
+++ scripts/dump-db.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 set -e
-cd /srv/udd.debian.org/udd/web/
+cd /srv/udd.debian.org/udd/web/dumps/
 umask 022
+# exclude dumps from backups, at DSA's request (ivodd 20140101)
+touch .nobackup
+
 pg_dump --no-owner -p 5452 -n history udd | gzip > udd-history.sql.gz.new
 mv udd-history.sql.gz.new udd-history.sql.gz
 pg_dump --no-owner -p 5452 -T ldap -T really_active_dds -T pts udd | gzip > udd.sql.gz.new
