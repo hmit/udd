147fe339444bd0a68b860273ccf40a1d7fb87b4b
Lucas Nussbaum <lucas@debian.org>
2014-06-30 15:39:31 +0200

    Mark the channel as updated at the max date of all items.

--- web/inc/feed.rb
+++ web/inc/feed.rb
@@ -7,15 +7,18 @@ class TodoFeed
           maker.channel.title = title
           maker.channel.description = '%s feed' % title
           maker.channel.link = "http://udd.debian.org/"
-          maker.channel.updated = Time.now.to_s
+          date = Time.at(0)
 
           items.each do |e|
               maker.items.new_item do |item|
                 item.link = e[:link]
                 item.title = e[:title]
-                item.updated = Time.now.to_s
+                d = e[:updated] || Time.now
+                date = d if d > date
+                item.updated = d.to_s
               end
           end
+          maker.channel.updated = date.to_s
         end
         puts "Content-type: text/xml\n\n"
         puts rss.to_s
