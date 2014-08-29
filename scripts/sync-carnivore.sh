280cc493ccd6762116ce0221da13274392d63561
Lucas Nussbaum <lucas@debian.org>
2014-08-28 19:11:37 -0700

    carnivore: Use HTTPS to reach qa.d.o

--- scripts/sync-carnivore.sh
+++ scripts/sync-carnivore.sh
@@ -4,4 +4,4 @@ set -e
 umask 002
 pw=$(</srv/udd.debian.org/udd-qa.debian.org-password)
 TARGET=/srv/udd.debian.org/mirrors/qa.debian.org-carnivore-report
-echo http://udd:${pw}@qa.debian.org/carnivore-report | wget -q -O $TARGET -i -
+echo https://udd:${pw}@qa.debian.org/carnivore-report | wget -q -O $TARGET -i -
