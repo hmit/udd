dc66e2f5fa116a04c0abf813af9d7690593af15c
Daniel Pocock <daniel@pocock.pro>
2015-02-03 18:31:40 +0100

    Re-apply "Initial iCalendar support. (Closes: #775180)"
    This reverts commit 04d933f072ae0a113e74a9169d1ff28570f26c74.

--- web/inc/page.rb
+++ web/inc/page.rb
@@ -3,6 +3,7 @@ require "erb"
 require 'oj'
 require 'yaml'
 require File.expand_path(File.dirname(__FILE__))+'/feed'
+require File.expand_path(File.dirname(__FILE__))+'/cal'
 
 class Page
   attr_accessor :data, :format, :template, :title, :feeditems
@@ -28,6 +29,8 @@ class Page
       puts Oj.load(Oj.dump(@data)).to_yaml
     elsif @format == 'rss'
       TodoFeed.new(@feeditems, @title)
+    elsif @format == 'ics'
+      TodoCalendar.new(@feeditems, @title)
     else
       content = File.read(File.expand_path(@template))
       t = ERB.new(content)
